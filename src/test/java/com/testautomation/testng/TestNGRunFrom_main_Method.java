775b61d9430abb595c5f4925b6792bd4d8b15df7
Sumon <sdtechie22@gmail.com>
2021-03-07 07:45:03 +0530

    Added TestNG execution from the main method

--- src/test/java/com/testautomation/testng/TestNGRunFrom_main_Method.java
+++ src/test/java/com/testautomation/testng/TestNGRunFrom_main_Method.java
@@ -1,6 +1,7 @@
 /*Running TestNG from the main() method of Java */
 package com.testautomation.testng;
 
+import java.util.ArrayList;
 import java.util.List;
 import org.testng.TestListenerAdapter;
 import org.testng.TestNG;
@@ -10,9 +11,8 @@ import com.beust.jcommander.internal.Lists;
 public class TestNGRunFrom_main_Method {
 
 	public static void main(String[] args) {
-		TestListenerAdapter TestListenerAdapter = new TestListenerAdapter();
 		TestNG testNG = new TestNG();
-		List<String> suites = Lists.newArrayList();
+		List<String> suites = new ArrayList<>();
 		suites.add("./src/test/java/com/testautomation/testng/testng1.xml");
 		suites.add("./src/test/java/com/testautomation/testng/testng2.xml");
 		suites.add("./src/test/java/com/testautomation/testng/testng3.xml");
